479599a3b4780cda15f44889f2324adc5d55f481
mbonilla <margot.bonilla@reaktor.com>
2020-08-22 17:44:20 +0200

    add tests

--- hard/17_17.py
+++ hard/17_17.py
@@ -23,7 +23,24 @@ def maxMinutes(appointments):
     return maxMinutesUtil(appointments, 0, memo)
 
 
+def maxMinutesIterative(appointments):
+    one_away = 0
+    two_away = 0
+    for i in range(len(appointments) - 1, -1, -1):
+        current = max(two_away + appointments[i], one_away)
+        two_away = one_away
+        one_away = current
+    return one_away
+
+
+
 if __name__ == "__main__":
     appointments = [30, 15, 60, 75, 45, 15, 15, 45]
     check(180, maxMinutes(appointments))
+    appointments = [75, 105, 120, 75, 90, 135]
+    check(330, maxMinutes(appointments))
+    appointments = [30, 15, 60, 75, 45, 15, 15, 45]
+    check(180, maxMinutesIterative(appointments))
+    appointments = [75, 105, 120, 75, 90, 135]
+    check(330, maxMinutesIterative(appointments))
 
